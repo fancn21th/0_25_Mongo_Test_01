7f7cc0e4f091e9024f0c55d591b784f1fef123d5
fancn <fanyj@yikeer.com>
2017-09-03 19:11:24 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 0_25_Mongo_Test_01
-Mongo DB Testing
+## Mongo DB Testing
+
+- run command below before running server.js
+~~~~
+rm -rf data && mkdir data && mongod --dbpth data
+~~~~
+
---
+++ server.js
@@ -0,0 +1,68 @@
+var async = require("async");
+var MongoClient = require("mongodb").MongoClient;
+var url = 'mongodb://localhost:27017/photo_app';
+var db;
+var { pix, a1, a2, a3 } = require('./mockdata');
+var albums_coll, photos_coll;
+
+
+async.waterfall([
+    function (cb) {
+        MongoClient.connect(
+            url,
+            {
+                poolSize: 100,
+                w: 1,
+            },
+            (err, dbase) => {
+                if (err) {
+                    console.log('bad!')
+                    process.exit(-1);
+                }
+
+                console.log("I have a connection!");
+                db = dbase;
+                cb(null);
+            }
+        );
+    },
+
+    function (cb) {
+        db.collection("albums", cb);
+    },
+
+    function (albums_obj, cb) {
+        albums_coll = albums_obj;
+        db.collection("photos", cb);
+    },
+
+    function (photos_obj, cb) {
+        photos_coll = photos_obj;
+        cb(null);
+    },
+
+    function (cb) {
+        albums_coll.insertMany([ a1, a2, a3 ], cb);
+    },
+
+    function (inserted_docs, cb) {
+        console.log("I inserted a document!!");
+        photos_coll.insertMany(pix, cb);
+    },
+
+    function (inserted_docs, cb) {
+        console.log("I inserted all my photos!");
+        cb(null)
+
+        // photos_coll.updateOne(
+        //     { filename: "photo_003.jpg", albumid: "australia2010"},
+        //     { $set: { description: "kangaroos" } },
+        //     cb);
+    },
+
+], function (err, results) {
+    console.log("Done!");
+    console.log(err);
+    console.log(results);
+    db.close();
+});
